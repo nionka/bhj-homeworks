b52832517dd8655447efce7b4d797c88b64078aa
Olga Runova <ockaaa@gmail.com>
2020-08-09 02:11:39 +0300

    second

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,11 +3,38 @@ const right = document.querySelector(".slider__arrow_next");
 const sliders = document.querySelectorAll(".slider__item");
 
 let count = 0;
-    right.onclick = () => {
-        sliders[count +1].className = "slider__item_active";
+    right.onclick = function() {
         sliders[count].className = "slider__item";
-        count++;
-        if (count === 4) {
-            count = 0;
-        }
-        }
+        count = ((count + 1) + sliders.length)%sliders.length;
+        console.log(count)
+        sliders[count].className = "slider__item_active";
+    }
+
+    left.onclick = function() {
+        sliders[count].className = "slider__item";
+        count = ((count - 1) + sliders.length)%sliders.length;
+        console.log(count)
+        sliders[count].className = "slider__item_active";
+    }
+
+// function nextSlide() {
+//     goToSlide(count + 1);
+// } 
+
+// function prevSlide() {
+//     goToSlide(count - 1);
+// }
+
+// function goToSlide(n) {
+//     sliders[count].className = "slider__item";
+//     count = (n + sliders.length)%sliders.length;
+//     sliders[count].className = "slider__item_active";
+// }
+
+// right.onclick = function() {
+//     nextSlide()
+// }
+
+// left.onclick = function() {
+//     prevSlide();
+// }
