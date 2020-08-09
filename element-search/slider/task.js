e1a349624d5bb490b9b21d6f6467b26c6cdb5e6f
Olga Runova <ockaaa@gmail.com>
2020-08-09 23:52:05 +0300

    fix added

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,38 +3,14 @@ const right = document.querySelector(".slider__arrow_next");
 const sliders = document.querySelectorAll(".slider__item");
 
 let count = 0;
-    right.onclick = function() {
-        sliders[count].className = "slider__item";
-        count = ((count + 1) + sliders.length)%sliders.length;
-        console.log(count)
-        sliders[count].className = "slider__item_active";
-    }
+right.onclick = function() {
+    sliders[count].classList.remove("slider__item_active");
+    count = ((count + 1) + sliders.length)%sliders.length;
+    sliders[count].classList.add("slider__item_active");
+}
 
-    left.onclick = function() {
-        sliders[count].className = "slider__item";
-        count = ((count - 1) + sliders.length)%sliders.length;
-        console.log(count)
-        sliders[count].className = "slider__item_active";
-    }
-
-// function nextSlide() {
-//     goToSlide(count + 1);
-// } 
-
-// function prevSlide() {
-//     goToSlide(count - 1);
-// }
-
-// function goToSlide(n) {
-//     sliders[count].className = "slider__item";
-//     count = (n + sliders.length)%sliders.length;
-//     sliders[count].className = "slider__item_active";
-// }
-
-// right.onclick = function() {
-//     nextSlide()
-// }
-
-// left.onclick = function() {
-//     prevSlide();
-// }
+left.onclick = function() {
+    sliders[count].classList.remove("slider__item_active");
+    count = ((count - 1) + sliders.length)%sliders.length;
+    sliders[count].classList.add("slider__item_active");
+}
