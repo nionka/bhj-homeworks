1ba4a71ea28cdbcf2b2f0b219964320e44ce8f20
Olga Runova <ockaaa@gmail.com>
2020-08-14 01:00:14 +0300

    added readbook

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -18,3 +18,44 @@ fontSize.forEach(value => {
         }
     })
 })
+
+const colorText = Array.from(document.querySelectorAll(".book__control_color .color"));
+const colorBG = Array.from(document.querySelectorAll(".book__control_background .color"));
+
+colorText.forEach(value => {
+    value.addEventListener("click", () => {
+        event.preventDefault();
+        document.querySelector(".book__control_color .color_active").classList.remove("color_active");
+        value.classList.add("color_active");
+
+        if (book.classList.contains("book_color-gray") || book.classList.contains("book_color-whitesmoke")) {
+            book.classList.remove("book_color-gray", "book_color-whitesmoke");
+        }
+
+        if (value.classList.contains("color_gray")) {
+            book.classList.add("book_color-gray");
+        } else if (value.classList.contains("color_whitesmoke")) {
+            book.classList.add("book_color-whitesmoke");
+        }
+    })
+})
+
+colorBG.forEach(value => {
+    value.addEventListener("click", () => {
+        event.preventDefault();
+        document.querySelector(".book__control_background .color_active").classList.remove("color_active");
+        value.classList.add("color_active");
+
+        if (book.classList.contains("book_bg-gray") || book.classList.contains("book_bg-black")) {
+            book.classList.remove("book_bg-gray", "book_bg-black");
+        }
+
+        if (value.classList.contains("color_gray")) {
+            book.classList.add("book_bg-gray");
+        } else if (value.classList.contains("color_black")) {
+            book.classList.add("book_bg-black");
+        }
+    })
+})
+
+
